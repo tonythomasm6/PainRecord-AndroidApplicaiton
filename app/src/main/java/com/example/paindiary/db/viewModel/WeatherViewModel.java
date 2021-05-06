31a38c706d3fee7e81f7757f0391a41fb4ea7b75
Tony Thomas <ttho0017@student.monash.edu>
2021-05-06 10:52:30 +1000

    Shared preference complete

--- app/src/main/java/com/example/paindiary/db/viewModel/WeatherViewModel.java
+++ app/src/main/java/com/example/paindiary/db/viewModel/WeatherViewModel.java
@@ -9,17 +9,10 @@ import java.util.HashMap;
 
 public class WeatherViewModel extends ViewModel {
 
-    private MutableLiveData<Double> temp;
-    private MutableLiveData<Double> humidity;
-    private MutableLiveData<Double> pressure;
-
     private MutableLiveData<HashMap<String,Double>> weather;
 
     public WeatherViewModel(){
         this.weather = new MutableLiveData<>();
-        this.temp = new MutableLiveData<>();
-        this.humidity = new MutableLiveData<>();
-        this.pressure = new MutableLiveData<>();
     }
 
 
@@ -32,27 +25,4 @@ public class WeatherViewModel extends ViewModel {
     }
 
 
-    public MutableLiveData<Double> getTemp() {
-        return temp;
-    }
-
-    public void setTemp(Double temp) {
-        this.temp.setValue(temp);
-    }
-
-    public MutableLiveData<Double> getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(Double humidity) {
-        this.humidity.setValue(humidity);
-    }
-
-    public MutableLiveData<Double> getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(Double pressure) {
-        this.pressure.setValue(pressure);
-    }
 }
